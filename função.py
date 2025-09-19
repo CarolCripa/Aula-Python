5cef67d41f25b6addfeeb47759d1005d4125ba4a
CarolCripa <carolcripa@gmail.com>
2025-09-19 17:39:46 -0300

    Aula Python (19-09-25)

---
+++ função.py
@@ -0,0 +1,13 @@
+def diga_ola():
+    print("Olá, Mundo!")
+
+diga_ola()
+
+def mensagem():
+    print("Seja bem-Vindo!")
+mensagem()
+
+def soma(a, b):
+    return a + b
+resultado = soma(5, 3)
+print(resultado)
